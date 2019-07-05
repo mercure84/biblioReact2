a296c2139331ae8c336b7621d99e82d16999dc8d
Mjuju84 <julien.marcesse@gmail.com>
2019-07-05 22:45:37 +0200

    prolongation de l'emprunt semble OK

--- src/Emprunts.js
+++ src/Emprunts.js
@@ -11,12 +11,41 @@ class Emprunts extends Component {
     super(props);
     this.Auth = new AuthService();
     this.afficherEmprunts();
+    this.prolonger = this.prolonger.bind(this);
+
   }
 
   state = {
     isLoaded: false,
     listeEmprunts: []
   };
+
+
+  async prolonger(emprunt){
+    console.log("On veut prolonger l'emprunt n° : " + emprunt.id);
+    this.setState({
+      isLoaded : false
+    })
+    const reponse = await fetch(
+      "http://localhost:8080/prolongerEmprunt/"+emprunt.id,{
+        method: "GET",
+        headers:{
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + this.Auth.getToken()
+        }
+
+      }
+    );
+
+    const resultat = await reponse.json();
+    this.setState({
+      isLoaded: true,
+  });
+  this.afficherEmprunts();
+    
+
+  }
 
   afficherEmprunts = async () => {
     const reponse = await fetch(
@@ -38,8 +67,9 @@ class Emprunts extends Component {
     });
   };
 
-  static dateOptions = {weekday: "long", year: "numeric", month: "long", day: "numeric"};
+  static dateOptions = {year: "numeric", month: "long", day: "numeric"};
 
+  
   render() {
     return (
       <div className="vue">
@@ -54,8 +84,8 @@ class Emprunts extends Component {
                   <th>Livre</th>
                   <th>Date d'emprunt</th>
                   <th>Date limite de restitution</th>
-                  <th>Prolonger</th>
-                  <th>Restitué ?</th>
+                  <th>Restitué</th>
+                  <th>Prolongé</th>
                 </tr>
               </thead>
               <tbody>
@@ -64,8 +94,9 @@ class Emprunts extends Component {
                     <td>{emprunt.livre.titre}</td>
                     <td>{new Date(emprunt.debutDate).toLocaleString('fr-FR', Emprunts.dateOptions)}</td>
                     <td>{new Date(emprunt.finDate).toLocaleString('fr-FR', Emprunts.dateOptions)}</td>
-                    <td>{emprunt.prolonge ? <span>Oui</span> : <span>Non</span>}</td>
                     <td>{emprunt.rendu ? <span>Oui</span> : <span>Non</span>}</td>
+                    <td><span>{emprunt.prolonge ? <span>Oui</span> : <span>Non</span>}</span><span> {!(emprunt.prolonge || emprunt.rendu) && <button onClick={()=>this.prolonger(emprunt)}>+4 sem.</button>}</span></td>
+
 
                   </tr>
                 ))}
